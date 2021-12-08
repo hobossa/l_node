c6acb022523e301527f7c1938a280d79bf8b9482
hobossa <hoboss@gmail.com>
2021-12-08 03:06:01 -0800

    use .env File

---
+++ BasicNodeAndExpress/boilerplate-express-gomix/myApp.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use('/public', express.static(__dirname+'/public'));
+
+app.get('/', function(req, res){
+    // res.send("Hello Express");
+    // console.log(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/json', (req, res) => {
+    console.log(process.env.MESSAGE_STYLE)
+    let resText = "Hello json"
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        resText = resText.toUpperCase();
+    } 
+    res.json({"message": resText});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
